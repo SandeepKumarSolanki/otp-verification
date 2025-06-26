7bf1a1538f3485f63dbdb78e6d1b878da80716de
SandeepKumar <sandeepkusolanki@gamil.com>
2025-06-26 17:41:13 +0530

    url

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 4000;
 // ✅ Await DB connection
 await connectDB();
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173','https://otp-verification-kappa.vercel.app'];
 
 app.use(express.json());
 app.use(cookieParser());
